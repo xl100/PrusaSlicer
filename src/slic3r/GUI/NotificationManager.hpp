6e0fed23ab5c97b4282bda048b2074e5843c2129
David Kocik <kocikdav@gmail.com>
2020-01-22 09:25:22 +0100

    cmake new files

--- src/slic3r/GUI/NotificationManager.hpp
+++ src/slic3r/GUI/NotificationManager.hpp
@@ -10,7 +10,12 @@ public:
 	notificationManager();
 	~notificationManager();
 private:
-	// stack-like structure for keeping notifications - we want to keep them, if they among x(~5) top then show them, if confirmed by user then delete them. When they arrive show them in notification pop-up(if clicked by user then no to keep them?).
+	// plan: stack-like structure for keeping notifications - we want to keep them, 
+	// if they are among top x(~5) then show them (probably in some openable panel/window), if confirmed by user then delete them.
+	// When they arrive show them in notification pop-up(if clicked by user then no to keep them?).
+	// notifications should be pushable for any component - should this be (partialy) static?
+	// should notifications "lead back"? after clicking show whats up. -needed only if doing "what gone wrong" notif
+	// example notifications: sliced, sd card removed
 }
 
 }//namespace GUI
