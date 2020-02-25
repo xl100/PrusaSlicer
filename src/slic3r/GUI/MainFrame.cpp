363dbe3e2bfcf42935347b8b18cc9c0aec772633
Enrico Turri <enricoturri@seznam.cz>
2020-02-25 13:33:00 +0100

    Another experiment to fix crash when closing on OSX 10.9.5

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -111,11 +111,13 @@ DPIFrame(NULL, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_S
         
         if(m_plater) m_plater->stop_jobs();
 
-        // Weird things happen as the Paint messages are floating around the windows being destructed.
-        // Avoid the Paint messages by hiding the main window.
-        // Also the application closes much faster without these unnecessary screen refreshes.
-        // In addition, there were some crashes due to the Paint events sent to already destructed windows.
-        this->Show(false);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//        // Weird things happen as the Paint messages are floating around the windows being destructed.
+//        // Avoid the Paint messages by hiding the main window.
+//        // Also the application closes much faster without these unnecessary screen refreshes.
+//        // In addition, there were some crashes due to the Paint events sent to already destructed windows.
+//        this->Show(false);
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
         // Save the slic3r.ini.Usually the ini file is saved from "on idle" callback,
         // but in rare cases it may not have been called yet.
